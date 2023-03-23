f256332e0885f34687cec03a3e35c3a709973412
abdulrahmandawoud <dawoud.abdulrahman.2004@gmail.com>
2023-03-23 23:39:16 +0600

    updated auth html templates

--- order/views.py
+++ order/views.py
@@ -2,35 +2,37 @@ from django.urls import reverse_lazy
 from django.views.generic import (CreateView, DeleteView, ListView,
                                   TemplateView, UpdateView)
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .forms import OrderForm
 from .models import Order
 
 
-class OrderCreateView(CreateView):
+class OrderCreateView(LoginRequiredMixin, CreateView):
     model = Order
     form_class = OrderForm
     template_name = "orders/order_create.html"
     success_url = reverse_lazy("orders-list")
 
 
-class OrderCreatedView(TemplateView):
+class OrderCreatedView(LoginRequiredMixin, TemplateView):
     template_name = "orders/order_created.html"
 
 
-class OrderListView(ListView):
+class OrderListView(LoginRequiredMixin, ListView):
     model = Order
     template_name = "orders/order_list.html"
     context_object_name = "orders"
 
 
-class OrderUpdateView(UpdateView):
+class OrderUpdateView(LoginRequiredMixin, UpdateView):
     model = Order
     form_class = OrderForm
     template_name = "orders/order_update.html"
     success_url = reverse_lazy("orders-list")
 
 
-class OrderDeleteView(DeleteView):
+class OrderDeleteView(LoginRequiredMixin, DeleteView):
     model = Order
     template_name = "orders/order_delete.html"
     success_url = reverse_lazy("orders-list")
