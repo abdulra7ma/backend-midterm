eb4f14c54113bb13ada4ac0ec679b990fb3d41cc
abdulrahmandawoud <dawoud.abdulrahman.2004@gmail.com>
2023-03-23 22:54:22 +0600

    completed order crud

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 from order import views
 
 urlpatterns = [
-    path("create-order", views.order_create, name="order-create"),
-    path("list", views.order_list,  name="orders-list"),
+    path("create", views.order_create, name="order-create"),
+    path("", views.order_list, name="orders-list"),
+    path("<int:pk>/update", views.order_update, name="order-update"),
+    path('<int:pk>/delete', views.order_delete, name='order-delete'),
+
 ]
--- order/views.py
+++ order/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import OrderForm
 from .models import Order
@@ -12,13 +12,31 @@ def order_create(request):
             return redirect("orders-list")
     else:
         form = OrderForm()
-    return render(request, "order_create.html", {"form": form})
+    return render(request, "orders/order_create.html", {"form": form})
 
 
 def order_created(request):
-    return render(request, "order_created.html")
+    return render(request, "orders/order_created.html")
 
 
 def order_list(request):
     orders = Order.objects.all()
-    return render(request, "order_list.html", {"orders": orders})
+    return render(request, "orders/order_list.html", {"orders": orders})
+
+
+def order_update(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    if request.method == "POST":
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect("orders-list")
+    else:
+        form = OrderForm(instance=order)
+    return render(request, "orders/order_update.html", {"form": form})
+
+
+def order_delete(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    order.delete()
+    return redirect('orders-list')
